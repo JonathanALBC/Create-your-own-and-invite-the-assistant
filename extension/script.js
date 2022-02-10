83d9cc4b788abe3401129b30536a20937e237e84
JonathanALBC <72008567+JonathanALBC@users.noreply.github.com>
2022-02-10 17:05:33 +0400

    Add files via upload

---
+++ extension/script.js
@@ -0,0 +1,5 @@
+//let arraySemester = [true, true, false, false, false, true, false];
+//arraySemester[0] = false; // disable the semester 0
+//showSemester(arraySemester);
+
+//document.querySelector("#breadcrumb") .style.display = 'none'
